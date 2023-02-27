c16be80ceee8038b7f9b27c910f3200e4a42ad6e
Oisin Hearne <oisinhearne@gmail.com>
2023-02-27 10:02:21 +0000

    data binding test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title: string = 'my-first-app';
+  name: string = "Oisin";
+  age: number = 20;
 }
